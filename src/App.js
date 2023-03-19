dd4db4672335c96b5031f0fabf5a34af95a8af0d
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-03-20 00:40:54 +0300

    Update -v1.0

--- src/App.js
+++ src/App.js
@@ -17,35 +17,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-
-
-
-import React, { useEffect } from 'react'
-import { reducerCases } from "./utils/Constants";
-import { useStateProvider } from "./utils/StateProvider";
-import Login from './components/New/Login';
-import Home from './components/New/Home';
-
-
-export default function App() {
-  const [{ token }, dispatch] = useStateProvider();
-  useEffect(() => {
-    const hash = window.location.hash;
-    if (hash) {
-      const token = hash.substring(1).split("&")[0].split("=")[1];
-      if (token) {
-        dispatch({ type: reducerCases.SET_TOKEN, token });
-      }
-    }
-    document.title = "Spotify";
-  }, [dispatch, token]);
-  return <div>
-            {
-            token ?
-                <Home /> : <Login />
-            }
-           </div>;
-*/
