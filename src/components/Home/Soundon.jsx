f42ab24a3249e29b8f9e876e51375088aeab4fd3
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-03-20 01:04:07 +0300

    Update -v1.0

--- src/components/Home/Soundon.jsx
+++ src/components/Home/Soundon.jsx
@@ -47,30 +47,31 @@ const UserProfile = () => {
   }, []);
 
     return (
-      <div className='flex flex-col bg-zinc-900 h-screen w-full bg-gradient-to-t from-gray-700 via-gray-900 to-black'>
+      <div className='flex flex-col bg-zinc-900 h-screen w-full bg-gradient-to-t items-center justify-center from-gray-700 via-gray-900 to-black'>
         {
           profile.display_name && profile.images && profile.followers.total && profile.uri && 
+          <div class="flex flex-col h-screen w-full">
             <header 
-              className='flex flex-cols-2 w-full h-72 text-base gap-10 items-center object-top
+              className='flex flex-cols-2 w-full h-72 text-base gap-10 items-center mt-20
                           bg-gradient-to-r from-pink-300 via-purple-400 to-indigo-400'
             >
               <div class="flex object-center h-full w-[400px] items-center justify">
                 <img className='flex w-[240px] ml-24 h-64 object-center rounded-full' src={profile.images[0].url} alt='profile img'/>
               </div>
               <div class="flex flex-col h-fit w-2/3 items-left object-left">
-                <h1 
+                  <h1 
                     class="flex text-blue-600 font-bold"
                   > 
-                    Hello 👋 This is your
-                    <a 
-                      href="https://open.spotify.com/user/berkink%C4%B1nay?si=308c415b81b84cda" 
-                      class="text-green-400 font-semibold ml-1 mr-1 hover:text-white"
-                      rel="noreferrer" 
-                      target="_blank"
-                    > 
-                      Spotify 
-                    </a> click here to follow him 🥰
-                      profile 
+                      Hello 👋 This is your
+                      <a 
+                        href="https://open.spotify.com/user/berkink%C4%B1nay?si=308c415b81b84cda" 
+                        class="text-green-400 font-semibold ml-1 mr-1 hover:text-white"
+                        rel="noreferrer" 
+                        target="_blank"
+                      > 
+                        Spotify 
+                      </a> click here to follow him 🥰
+                        profile 
                   </h1>
                   <h2 className='text-white font-bold'>
                     your account name: {profile.display_name} 🙂
@@ -90,6 +91,10 @@ const UserProfile = () => {
                   </div>
               </div>
           </header>
+          <div class='flex w-full h-screen items-center justify-center'>
+            <h1 class="font-bold text-2xl text-white">SPOTIFY USER LOGIN DEMO</h1>
+          </div>
+        </div>
        }
       </div>
   )
